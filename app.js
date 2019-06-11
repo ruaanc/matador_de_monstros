744797033e227b80b5bfcb6edc45272a5b818d62
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-06-11 20:30:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,24 @@ new Vue({
                         this.runnig = true,
                         this.vida_jogador = 100,
                         this.vida_monstro = 100
+                },
+                attack(especial) {
+                      this.hurt('vida_jogador' ,7, 12, false) 
+                      this.hurt('vida_monstro' ,7, 10, especial) 
+                },
+                hurt(atr ,min, max, especial) {
+                        const plus = especial ? 5 : 0,
+                        hurte = this.getRandom(min + plus, max + plus)
+                        this[atr] = Math.max(this[atr] - hurte, 0)
+                },
+                getRandom(min, max) {
+                        const value = Math.random() * (max - min) + min
+                        return Math.round(value)
                 }
         },
         watch: {
-
+                hasResult(value) {
+                        if (value) this.runnig = false
+                }
         }
 });
